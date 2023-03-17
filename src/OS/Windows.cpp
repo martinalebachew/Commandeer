6e2aad88f7d92f8d473ac549930f710cf9b2dbc9
Martin Alebachew <martinalebachew@gmail.com>
2023-03-17 16:55:35 +0200

    Implemented Windows::KillProcessByExecName
    - Added protected method Windows::GetProcessHandlesByName.
    - Implemented Windows::KillProcessByExecName.

--- src/OS/Windows.cpp
+++ src/OS/Windows.cpp
@@ -1,11 +1,49 @@
 // Windows.cpp
 // (C) Martin Alebachew, 2023
 
+#include <vector>
 #include <Windows.h> // Win32 API
+#include <tlhelp32.h>
 #include "Windows.h" // 'Windows' class header file
 
+std::vector<HANDLE> Windows::GetProcessHandlesByName(std::string execName) {
+	std::vector<HANDLE> hProcesses;
+	PROCESSENTRY32W entry;
+	entry.dwSize = sizeof(PROCESSENTRY32W);
+	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+
+	wchar_t* wExecName = new wchar_t[execName.length() + 1];
+	for (int i = 0; i < execName.length(); i++)
+		wExecName[i] = execName[i];
+	wExecName[execName.length()] = NULL; // Null termination character
+
+	while (Process32Next(snapshot, &entry)) {
+		if (_wcsicmp((WCHAR*)entry.szExeFile, wExecName) == 0)
+			hProcesses.push_back(OpenProcess(PROCESS_ALL_ACCESS, FALSE, entry.th32ProcessID));
+	}
+
+	CloseHandle(snapshot);
+	return hProcesses;
+}
+
 bool Windows::KillProcessByExecName(std::string execName) {
-	std::cout << "Killing process by name: " << execName << std::endl;
+	std::vector<HANDLE> hProcesses = GetProcessHandlesByName(execName);
+
+	if (!hProcesses.size()) {
+		std::cout << "Failed to locate processes of " << execName << std::endl;
+		return false;
+	}
+
+	for (int i = 0; i < hProcesses.size(); i++) {
+		HANDLE hProcess = hProcesses[i];
+
+		if (hProcess && TerminateProcess(hProcess, 0)) {
+			WaitForSingleObject(hProcess, INFINITE); // Wait until process terminates
+			CloseHandle(hProcess);
+		}
+	}
+
+	std::cout << "Terminated all processes of " << execName << std::endl;
 	return true;
 }
 
@@ -35,12 +73,12 @@ bool Windows::LaunchBrowserExec(std::string execPath, std::string flags) {
 
 		if (FormatMessageA(
 			FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_IGNORE_INSERTS, // Formatting options
-			NULL,                    // Argument is irrelevant
-			errorCode,                   // Specify requested message id as the error id
-			0x409,                   // Retrieve error in en-US
-			(LPSTR)&errorMessage,    // Pointer to the message buffer, out
-			0,                       // Minumum size of the message buffer in TCHARs
-			NULL                     // Argument is irrelevant (no inserts)
+			NULL,                   // Argument is irrelevant
+			errorCode,              // Specify requested message id as the error id
+			0x409,                  // Retrieve error in en-US
+			(LPSTR)&errorMessage,   // Pointer to the message buffer, out
+			0,                      // Minumum size of the message buffer in TCHARs
+			NULL                    // Argument is irrelevant (no inserts)
 		))
 			std::cout << errorMessage << std::endl;
 		else
@@ -48,7 +86,7 @@ bool Windows::LaunchBrowserExec(std::string execPath, std::string flags) {
 
 		return false;
 	}
-		
+	
 	std::cout << "Browser process created." << std::endl;
 	
 	// *** Uncomment to wait for process termination and print exit code ***
