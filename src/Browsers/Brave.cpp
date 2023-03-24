e18bb96a4941da4ec2249ab2d6a2dc219f8a2bfc
Martin Alebachew <martinalebachew@gmail.com>
2023-03-24 10:14:39 +0300

    Headless Attack! 😈
    - Added the '--headless=new' flag when launching Brave. Chromium-based browsers seem to dump user cookies only in the new headless mode.
    - Read more about the new headless mode (released on Feb 2023): https://developer.chrome.com/articles/new-headless/

--- src/Browsers/Brave.cpp
+++ src/Browsers/Brave.cpp
@@ -9,5 +9,5 @@
 Brave::Brave(OS* os) :
 	Browser((std::string)"Brave.exe",
 		(std::string)R"(C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe)",
-		(std::string)R"(--remote-debugging-port=9222 --user-data-dir="C:\Users\martin\AppData\Local\BraveSoftware\Brave-Browser\User Data")",
+		(std::string)R"(--remote-debugging-port=9222 --headless=new --user-data-dir="C:\Users\martin\AppData\Local\BraveSoftware\Brave-Browser\User Data")",
 		os) { }
