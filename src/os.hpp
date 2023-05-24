68470ac5a4f2bc0b57fa258e68503749adf153f4
Martin Alebachew <martinalebachew@gmail.com>
2023-05-24 23:15:52 +0300

    Changed OS class declaration
    - Allocated objects on stack in main program.
    - Changed OS class declaration to match the new structure.

--- src/os.hpp
+++ src/os.hpp
@@ -6,7 +6,7 @@
 
 class OS {
 public:
-	virtual bool KillProcessByExecName(std::string execName) const = 0;
-	virtual bool LaunchBrowserExec(std::string execPath, std::string flags) const = 0;
-	virtual void Wait(unsigned int seconds) const = 0;
+	bool KillProcessByExecName(std::string execName) const;
+	bool LaunchBrowserExec(std::string execPath, std::string flags) const;
+	void Wait(unsigned int seconds) const;
 };
